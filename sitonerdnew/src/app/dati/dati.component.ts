6a96345924f77559249509a5fc0b387b4dd83d92
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2024-11-22 08:42:46 +0000

    terzo

--- sitonerdnew/src/app/dati/dati.component.ts
+++ sitonerdnew/src/app/dati/dati.component.ts
@@ -1,48 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-dati',
   templateUrl: './dati.component.html',
   styleUrls: ['./dati.component.css']
 })
 export class DatiComponent implements OnInit {
-    datiCarta: any;
-    caricamento = false;
+  datiCarta: any;
+  caricamento = false;
 
-    
-    constructor(private route: ActivatedRoute, private http:HttpClientModule) {
-      
-    }
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
-    ngOnInit(): void {
-      this.route.paramMap.subscribe(params=>{
-        const datiCarta = params.get('name')!;
-        this.CercaCarta(datiCarta);
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const nomeCarta = params.get('name')!;
+      this.CercaCarta(nomeCarta);
+    });
+  }
 
-      });
-      
-    }
-  
-  
-  CercaCarta(datiCarta: string): void{
-    this.caricamento= true;
-    const url = 'https://api.scryfall.com/cards/named?fuzzy=${datiCarta}';
+  CercaCarta(nomeCarta: string): void {
+    this.caricamento = true;
+    const url = `https://api.scryfall.com/cards/named?fuzzy=${nomeCarta}`;
     this.http.get(url).subscribe(
-      (data)=>{
+      (data) => {
         this.datiCarta = data;
-        this.caricamento= false;
-
+        this.caricamento = false;
       },
-
-      (error)=>{
-        console.error('suca')
-        this.caricamento=false;
+      (error) => {
+        console.error('Errore nel caricamento dei dati:', error);
+        this.caricamento = false;
       }
-    
     );
   }
-    
-   
-
 }
